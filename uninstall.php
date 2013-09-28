b670d0454fc50c6f5b28489335a1cb65c2c110e9
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2013-09-27 20:09:48 -0400

    removed security calls in uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,11 @@
 <?php
 // Zen Dash uninstall script
 // deletes all database options when plugin is removed
+// as discussed here: http://codex.wordpress.org/Function_Reference/register_uninstall_hook
 // @since 1.0
 // 
+
+/*
 // Direct calls to this file are Forbidden when core files are not present
 // Thanks to Ed from ait-pro.com for this  code 
 
@@ -17,6 +20,8 @@ header('Status: 403 Forbidden');
 header('HTTP/1.1 403 Forbidden');
 exit();
 }
+*/
+
 // if uninstall is not called from WordPress then exit
 if (!defined('WP_UNINSTALL_PLUGIN')) exit();
 
@@ -53,7 +58,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) exit();
 	delete_option ('zendash_footer_version');
 	
 
-// Thanks for using Zen Sash
+// Thanks for using Zen Dash
 // If you'd like to try again someday check out http://wpguru.co.uk where it lives and grows
 
 ?>
